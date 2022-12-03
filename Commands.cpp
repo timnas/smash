dcfbd4d9f6cc580692a5aff37aa32315a86d3eb1
timnas <timna.smadja@gmail.com>
2022-12-03 15:59:06 +0200

    changed fg command

--- Commands.cpp
+++ Commands.cpp
@@ -526,15 +526,14 @@ void ForegroundCommand::execute() {
         smash.curr_fg_pid = job_pid;
         smash.current_cmd = job->command;
         smash.fg_jid = job_id;
-        if (waitpid(job_pid, nullptr, WUNTRACED) != job_pid) {
+        smash.jobs_list.removeJobById(job_id);
+        int stat_loc;
+        if (waitpid(job_pid, &stat_loc, WUNTRACED) == FAIL) {
             perror("smash error: waitpid failed");
             freeArgs(args, num_of_args);
             return;
         }
-        smash.jobs_list.removeJobById(job_id);
         smash.fg_jid = EMPTY;
-        smash.curr_fg_pid = EMPTY;
-        return;
     }
 
     else if (num_of_args == 2) { //Bring wanted job to foreground
@@ -556,17 +555,20 @@ void ForegroundCommand::execute() {
             }
             // now job is in backround
             cout << job->command << " : " << job->jobPid << endl;
+
+            //       smash.current_process_pid = job_pid;
             smash.curr_fg_pid = job_pid;
             smash.current_cmd = job->command;
             smash.fg_jid = job_id;
-            if (waitpid(job_pid, nullptr, WUNTRACED) != job_pid) {
+            smash.jobs_list.removeJobById(job_id);
+
+            int stat_loc;
+            if (waitpid(job_pid, &stat_loc, WUNTRACED) == FAIL) {
                 perror("smash error: waitpid failed");
                 freeArgs(args, num_of_args);
                 return;
             }
-            smash.jobs_list.removeJobById(job_id);
             smash.fg_jid = EMPTY;
-            smash.curr_fg_pid = EMPTY;
         }
         else {
             cerr << "smash error: fg: job-id " << job_id << " does not exist" << endl;
